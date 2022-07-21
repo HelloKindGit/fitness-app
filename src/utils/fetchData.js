4fda6073434784da7fe5c6b38ae41e1e9d027c64
HelloKindGit <dominik.wache@web.de>
2022-07-21 14:04:18 +0200

    BodyPart, Detail, ExerciseCard, Exercises, ExerciseVideos, Footer, HeroBanner, HorizontalScrollbar, Loader, Navbar, SearchExercises, SimilarExercises, ExerciseDetails, Home, fetchData

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ae6fa88ca9msh4d4215e18e58238p1ab093jsnfc9a5e62580f',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ae6fa88ca9msh4d4215e18e58238p1ab093jsnfc9a5e62580f',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
